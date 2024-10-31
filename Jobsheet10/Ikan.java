23a8925f479831e245b98509153de1c48fd08202
aqilaazza <aqilanurazza123@gmail.com>
2024-10-31 08:12:13 +0700

    update

---
+++ Jobsheet10/Ikan.java
@@ -0,0 +1,10 @@
+package Jobsheet10;
+
+public class Ikan extends Hewan {
+    
+    
+    @Override
+    public void bergerak() {
+        System.out.println("Berjalan dengan SIRIP, \"wush..wush..\"");
+    }
+}
